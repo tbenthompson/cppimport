7e6907237fd2b974dcfe8450a65022ffad8fd55f
Ben Thompson <t.ben.thompson@gmail.com>
2016-06-23 15:43:18 -0400

    Minor release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     },
 
     name = 'cppimport',
-    version = '16.06',
+    version = '16.06.23',
     description = 'Import C++ files directly from Python!',
     long_description = description,
     url = 'https://github.com/tbenthompson/cppimport',
