42f9bca89372d194cb68bde3c51601d8fffce8ec
Ben Thompson <t.ben.thompson@gmail.com>
2017-07-18 22:55:21 -0400

    turn off strict prototypes warning

--- cppimport/config.py
+++ cppimport/config.py
@@ -14,3 +14,10 @@ def quiet_print(a):
     global quiet
     if not quiet:
         print(a)
+
+def turn_off_strict_prototypes():
+    import distutils.sysconfig
+    cfg_vars = distutils.sysconfig.get_config_vars()
+    for key, value in cfg_vars.items():
+        if type(value) == str:
+            cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
--- setup.py
+++ setup.py
@@ -23,26 +23,21 @@ setup(
     version = '16.06.24',
     description = 'Import C++ files directly from Python!',
     long_description = description,
+
     url = 'https://github.com/tbenthompson/cppimport',
     author = 'T. Ben Thompson',
     author_email = 't.ben.thompson@gmail.com',
     license = 'MIT',
     platforms = ['any'],
     classifiers = [
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Operating System :: POSIX',
         'Topic :: Software Development',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: C++'
     ]
 )
