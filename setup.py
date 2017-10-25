242bb9a5b0ed80b92c1f9dbae9e577e16364eba4
Ben Thompson <t.ben.thompson@gmail.com>
2017-10-25 02:26:54 -0400

    17.10.25

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     },
 
     name = 'cppimport',
-    version = '17.09.18',
+    version = '17.10.25',
     description = 'Import C++ files directly from Python!',
     long_description = description,
 
