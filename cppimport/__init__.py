1602f256dedf098345930b9af4228fc32d1c43d9
Ben Thompson <t.ben.thompson@gmail.com>
2017-12-01 19:46:46 -0500

    add set_rtld_flags to __init__.py

--- cppimport/__init__.py
+++ cppimport/__init__.py
@@ -1,4 +1,4 @@
-from cppimport.config import set_quiet, force_rebuild, file_exts, turn_off_strict_prototypes
+from cppimport.config import set_quiet, force_rebuild, file_exts, turn_off_strict_prototypes, set_rtld_flags
 from cppimport.importer import imp, imp_from_filepath
 from cppimport.templating import setup_pybind11
 
