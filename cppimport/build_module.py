0e3b34e1f98122f613566cfeecb7ae21bde07e3a
Stefano Rivera <stefanor@debian.org>
2022-09-29 12:40:42 +0200

    Import distutils after setuptools
    distutils is being removed from Python in 3.12. To prepare for this,
    setuptools now bundles its own copy of distutils, which it
    patches into sys.path, by default, since setuptools 60.
    
    https://setuptools.pypa.io/en/latest/history.html#v60-0-0
    
    To avoid importing two different distutils, import setuptools before
    distutils.
    
    Bug-Debian: https://bugs.debian.org/1020571

--- cppimport/build_module.py
+++ cppimport/build_module.py
@@ -1,6 +1,4 @@
 import contextlib
-import distutils
-import distutils.sysconfig
 import io
 import logging
 import os
@@ -9,6 +7,8 @@ import tempfile
 
 import setuptools
 import setuptools.command.build_ext
+import distutils
+import distutils.sysconfig
 
 import cppimport
 from cppimport.filepaths import make_absolute
